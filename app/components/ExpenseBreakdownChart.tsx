90465a0fa9123b04ebdb11ebc52a28f3c28ec96c
a <adamgustin@me.com>
2026-02-06 12:33:08 -0500

    slider back to w-full

--- app/components/ExpenseBreakdownChart.tsx
+++ app/components/ExpenseBreakdownChart.tsx
@@ -194,7 +194,7 @@ export default function ExpenseBreakdownChart({ data, loading = false, onExpense
           max={Math.ceil(maxValue)}
           value={minThreshold}
           onChange={(e) => setMinThreshold(Number(e.target.value))}
-          className="w-1/2"
+          className="w-full"
         />
       </div>
 
